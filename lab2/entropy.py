b7581f712b9e2e722d11f547175f333cdfc2bb1b
Shiv Sulkar <ssulkar@calpoly.edu>
2018-04-22 23:27:33 -0700

    added entropy function

--- lab2/entropy.py
+++ lab2/entropy.py
@@ -1,16 +1,21 @@
 import random
 
-def entropy(D):
-    if (len(D) == 0):
-        return 0;
+def entropy(dataSet):
+    e = 0 #when dataSet is empty the entropy is 0
+    if len(dataSet) != 0:
+        classList = list(set([d[-1] for d in dataSet])) #get classes and remove duplicates
+        classCount = [0 for i in classList]
+        for d in dataSet:
+            classCount[classList.index(d[-1])] += 1 #increment the numerator
+        
+        for n in classCount:
+            n = n/len(dataSet) #divide by the dataSet size
+            e += n * log (n)
     
-    classCount = []
-    for i in range(len(D)):
-        pass        
+    return -1*e
+    
 
-    val = []
-    for i in range(len(D)):
-        val[i] = log(D[i])
+   
 
 def log(k):
     val = 0
