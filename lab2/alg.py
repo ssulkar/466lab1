890ff96ba0bed81b64d40363db3ac2d617d288e2
devmart10 <devmart10@gmail.com>
2018-04-30 10:10:48 -0700

    cleans up algorithm

--- lab2/alg.py
+++ lab2/alg.py
@@ -1,8 +1,6 @@
 import random
 import math
 import logging
-logging.basicConfig(level=logging.DEBUG)
-# logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 import d_tree
@@ -13,7 +11,7 @@ def c45(D, A, T, threshold):
     # TODO these can be in the same if statement
     if homogenous:
         n = d_tree.Node(dominant)
-        logger.debug('homogenous -> created node: %s' % dominant)
+        logger.debug('homogenous (%d) -> created node: %s' % (len(D), dominant))
         return n
     # no more attributes to split on, so pick dominant classification
     elif len(A) == 0:
@@ -21,20 +19,19 @@ def c45(D, A, T, threshold):
         logger.debug('no attributes -> created node: %s' % dominant)
         return n
     else:
-        splitAttr = selectSplittingAttribute(A, D, threshold)
-        if splitAttr == None:
+        splitIndex = selectSplittingAttribute(D, A, threshold)
+        if splitIndex == None:
             n = d_tree.Node(dominant)
             logger.debug('no split -> created node: %s' % dominant)
             return n
 
-        splitIndex = A.index(splitAttr)
-        A.remove(splitAttr) # removes attribute from future consideration
-        
-        splitData = {}
-        for d in D:
-            splitData[d[splitIndex]] = splitData.get(d[splitIndex], [])
-            splitData[d[splitIndex]].append(d)
+        # TODO split data is different each time because its a dictionary
+        # shouldn't matter though right? why does the order of processing
+        # the tree?
+        splitData = split(D, A, splitIndex)
 
+        splitAttr = A.pop(splitIndex)[0] # removes attribute from future consideration
+        logger.debug('splitting on: %s' % splitAttr)
         n = d_tree.Node(splitAttr)
         for k, v in splitData.items():
             if len(v) != 0:
@@ -42,6 +39,63 @@ def c45(D, A, T, threshold):
                 e = d_tree.Edge(k, c45(v, A, T, threshold))
                 n.edges.append(e)
         return n
+
+def selectSplittingAttribute(D, A, threshold):
+    p0 = entropy(D)
+    gain = [0 for a in A]
+    for k, v in A:
+        # check for columns to skip
+        if v != -1:
+            index = A.index([k, v])
+            gain[index] = p0 - entropyAttr(D, A, index)
+    
+    # logger.debug('A[]: %s' % [x[0] for x in A])
+    # logger.debug('gain[]: %s' % gain)
+    logger.debug('max(gain): %s' % max(gain))
+    bestIndex = gain.index(max(gain))
+    
+    if gain[bestIndex] > threshold :
+        # logger.debug('selected splitting attribute: %s' % A[bestIndex])
+        return bestIndex
+    else:
+        return None
+
+def entropyAttr(D, A, splitIndex):
+    # split into subsets
+    splitData = split(D, A, splitIndex)
+    lenD = len(D)
+    # find entropy of each subset
+    entropies = []
+    for k, v in splitData.items():
+        # weight each value by it's length. len(subset)/len(all)
+        e = entropy(v)
+        entropies.append(float(len(v)/lenD)*e)
+    # add together
+    return sum(entropies)
+
+def entropy(D):
+    e = float(0) #when D is empty the entropy is 0
+    if len(D) != 0:
+        classList = list(set([d[-1] for d in D])) #get classes and remove duplicates
+        classCount = [float(0) for i in classList]
+        for d in D:
+            classCount[classList.index(d[-1])] += 1 #increment the numerator
+        
+        for n in classCount:
+            n = n/len(D) #divide by the D size
+            e += n * log(n)
+    
+    return -1*e
+
+# splits given data based on given attribute index
+# need to return an array, but might want to use dictionary for easy sorting
+# oh but can't because answers are not deterministic with dictionary
+def split(D, A, splitIndex):
+    splitData = {}
+    for d in D:
+        splitData[d[splitIndex]] = splitData.get(d[splitIndex], [])
+        splitData[d[splitIndex]].append(d)
+    return splitData
 
 # returns dominant class and whether it is homogenous
 def getDomClass(D):
@@ -57,51 +111,11 @@ def getDomClass(D):
             cur_class = k
     return cur_class, cur_largest == len(D)
 
-def getClass(data):
-    return data[-1]
-
-def entropy(dataSet):
-    e = float(0) #when dataSet is empty the entropy is 0
-    if len(dataSet) != 0:
-        classList = list(set([d[-1] for d in dataSet])) #get classes and remove duplicates
-        classCount = [float(0) for i in classList]
-        for d in dataSet:
-            classCount[classList.index(d[-1])] += 1 #increment the numerator
-        
-        for n in classCount:
-            n = n/len(dataSet) #divide by the dataSet size
-            e += n * log (n)
-    
-    return -1*e
-
 def log(k):
     val = 0
     if k!=0:
         val = math.log(k,2)
     return val
 
-#TODO
-def selectSplittingAttribute (A, D, threshold):
-    logger.debug('len(D): %s' % len(D))
-    p0 = entropy(D)
-    gain = [0 for a in A]
-    for i in range(len(gain)):
-        gain[i] = p0 - entropy(A[i])
-        # debug
-        gain[i] *= -1
-    
-    # logger.debug('gain[]: %s' % gain)
-    logger.debug('max(gain): %s' % max(gain))
-    bestIndex = gain.index(max(gain))
-    
-    if gain[bestIndex] > threshold :
-        logger.debug('selected splitting attribute: %s' % A[bestIndex])
-        return A[bestIndex]
-    else:
-        return None    
-    '''
-    if len(A) < 3:
-        return None
-    else:
-        return A[random.randint(1, len(A) - 1)]
-    '''
+def getClass(data):
+    return data[-1]
--- lab2/main.py
+++ lab2/main.py
@@ -3,6 +3,10 @@ import csv
 import json
 import xml.etree.ElementTree
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.INFO)
+
 import alg
 import d_tree
 
@@ -15,21 +19,21 @@ def main():
         reader = csv.reader(f, delimiter=',')
         data = [row for row in reader]
     attributes = data[0]
-    # attributes = attributes[1:-1]
+    A = [0 for x in range(len(data[0]))]
+    for i in range(len(data[0])):
+        A[i] = [attributes[i], int(data[1][i])]
+
+    A[-1][1] = -1
     data = data[3:]
-    T = alg.c45(data, attributes, d_tree.Node('root'), 1.0)
-    # print(json.dumps(d_tree.toJSON(T), sort_keys=True, indent=4))
+
+    threshold = .01
+    T = alg.c45(data, A, d_tree.Node('root'), threshold)
+    
     xml = d_tree.toXML(d_tree.Tree(T))
     print(xml)
     # tmp = ElementTree.fromstring(xml)
     # tree = ElementTree.ElementTree(tmp)
     # tree.write('output.xml')
-
-def log(x):
-    val = 0
-    if x != 0:
-        val = math.log(x,2)
-    return val
 
 if __name__=='__main__':
     main()
