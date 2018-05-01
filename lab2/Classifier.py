c83552568236dc945bb59e85adcf648b24276ec6
devmart10 <devmart10@gmail.com>
2018-05-01 15:40:21 -0700

    cleans up files

--- lab2/Classifier.py
+++ lab2/Classifier.py
@@ -1,7 +1,15 @@
 import sys
+import csv
+import json
+import xml.etree.ElementTree as ET
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def main(args):
-    pass
+    logger.debug('args: %s' % args)
 
 if __name__=='__main__':
     if len(sys.argv) < 3:
