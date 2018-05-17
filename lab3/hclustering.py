7f6168848eeeb29e3bf008094f9fbb6e29a37b9a
Shiv Sulkar <ssulkar@calpoly.edu>
2018-05-16 20:17:50 -0700

    Can't figure out how to output the xml or what the threshold option is supposed to do

--- lab3/hclustering.py
+++ lab3/hclustering.py
@@ -1,33 +1,38 @@
 import sys
 import csv
 import math
+import numpy as np
 from heapq import nsmallest
 
 def main(args):
-    testingstuff()
-    '''if (len(args) < 2 or len(args) > 3):
+    if (len(args) < 2 or len(args) > 3):
         print("Usage: python3 hclustering <Filename> [<threshold>]")
     else: 
         filename = args[1]
+        
+        #Threshold???
         threshold = 0
-
+        if(len(args) == 3):
+            threshold = args[2]
+        
         rows = []
         with open(filename, 'r') as f:
-           reader = csv.reader(f)
-           rows = [[int(i) for i in r] for r in reader]
+            reader = csv.reader(f)
+            header = next(reader)
+            rows = [[float(data) for i, data in enumerate(r) if header[i] != '0'] for r in reader if len(r) > 0]
         
-        if (len(args) == 3):
-	    threshold = args[2]'''
-
-def testingstuff():
-    D = [[2,2],[2,3],[3,4],[2,1],[6,10],[1,4],[5,1]]
-    #print(createDistanceMatrix(C))
-    #print(nsmallest(1,createDistanceMatrix(C)[0]))
-    #print(getMinimumValue(createDistanceMatrix(C)))
-    agglomerative(D))
-
+        C = agglomerative(rows)
+        
+        #print XML???
+        printXML(C)
+        
+# distance between n-D points
 def distanceFormula(a, b):
-    return (math.sqrt(((a[0] - b[0])**2) + ((a[1] - b[1])**2)))
+    #return (math.sqrt(((a[0] - b[0])**2) + ((a[1] - b[1])**2)))
+    sum = 0
+    for i in range(len(a)):
+        sum += (a[i]-b[i])**2
+    return math.sqrt(sum)
     
 def initializeDistanceMatrix(dimension):
     distanceMatrix = [[0 for i in range(dimension)] for j in range(dimension)]
@@ -45,28 +50,43 @@ def argMin (distanceMatrix):
             elif (currentValue != 0 and currentValue < smallestValue):
                 smallestValue = currentValue
                 row, col = i, j
-    return row, col    
+    return row, col, smallestValue
 
-#def averageLinkMethod():
-def agglomerative(D):
-    C = D[:]
-    i = 1
-    while len(C) > 1:
-        distanceMatrix = initializeDistanceMatrix(len(C))
-        for j in range(len(C)):
-            for k in range(j+1, len(C)):
-                distanceMatrix[j][k] = distanceFormula(C[j], C[k])
-        s, r = argMin(distanceMatrix)
-        #TODO need to use averageLink to merge clusters.
-        '''for j in range(len(C)-1):
-            if j != r and j != s:
-                C[j+1] = C[j]
-            elif j == r:
-                C[j] = C[r], C[s]'''
-        print (distanceMatrix)
-        print (s, r)
-        break
+def singleLinkMethod(distanceMatrix, s, r):
+    for i in range(s):
+        distanceMatrix[s][i] = min(distanceMatrix[s][i], distanceMatrix[r][i])
+        distanceMatrix[i][s] = min(distanceMatrix[i][s], distanceMatrix[i][r])
+    #trim row r
+    distanceMatrix.pop(r)
+    #trim col r
+    for i in range(len(distanceMatrix)):
+        distanceMatrix[i].pop(r)
     
+    return distanceMatrix
+        
+
+def agglomerative(D):
+    C = [(tuple(point)) for point in D]
+    
+    # create distanceMatrix
+    distanceMatrix = initializeDistanceMatrix(len(C))
+    for j in range(len(C)):
+        for k in range(j+1, len(C)):
+            distanceMatrix[j][k] = distanceFormula(C[j], C[k])
+      
+    while len(C) > 1:
+        # indexes of two closest clusters
+        s, r, height = argMin(distanceMatrix)
+        # recalculate distance matrix
+        distanceMatrix = singleLinkMethod(distanceMatrix, s, r)
+        # merge clusters
+        C[s] = [C[s], C[r], height]
+        C.pop(r)
+    return C
+ 
+def printXML (C):
+    print(C)
+        
 if __name__ == '__main__':
     args = sys.argv
     main(args)
