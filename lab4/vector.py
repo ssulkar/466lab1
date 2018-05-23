882581bcbc691eb5f5a23c537ba90d52281b78a6
devmart10 <devmart10@gmail.com>
2018-05-22 23:08:00 -0700

    fixes some things

--- lab4/vector.py
+++ lab4/vector.py
@@ -1,5 +1,7 @@
 import math
 
+import numpy as np
+
 class Vector:
     """A class for storing tf-idf vector representations of text documents
 
@@ -43,48 +45,53 @@ class Vector:
         """The similarity of two vectors using okapi"""
         return 0
 
-def compute_TF_IDF(tf_dict, idf_dict):
-    # might need to initialize this
-    tf_idf_dict = {}
-    for word, val in tf_dict.items():
-        tf_idf_dict[word] = val * idf_dict[word]
-    return tf_idf_dict
+def compute_TF_IDF(tf_table, idf_table):
+    tf_idf_table = np.zeros(shape=tf_table.shape)
+    for i, doc in enumerate(tf_table):
+        for j, term in enumerate(doc):
+            tf_idf_table[i][j] = term * idf_table[j]
+    return tf_idf_table
 
-def compute_TF(doc):
-    tf_dict = _make_word_dict(doc)
-    # normalize each count
-    max_count = max(tf_dict.values())
-    for word, count in tf_dict.items():
-        tf_dict[word] = _normalize(count, max_count)
-    return tf_dict
+def compute_TF_normalized(doc_list, vocab):
+    tf_table = compute_TF(doc_list, vocab)
+    tf_table = [[_normalize(data, max(row)) for data in row] for row in tf_table]
+    return np.array(tf_table)
 
-def _make_word_dict(bag):
-    wordDict = {}
-    for word in set(bag):
-        wordDict[word] = bag.count(word)
-    return wordDict
+def compute_TF(doc_list, vocab):
+    rows = len(doc_list)
+    cols = len(vocab)
+
+    tf_table = np.zeros(shape=(rows, cols))
+    for i, doc in enumerate(doc_list):
+        for j, term in enumerate(vocab):
+            tf_table[i][j] = _get_term_count(term, doc)
+    return tf_table
+
+def _get_term_count(term, doc):
+    return doc.count(term)
 
 def _normalize(x, max):
     return float(x / max)
 
 def get_vocabulary(doc_list):
-    vocab = set()
+    vocab = []
     for doc in doc_list:
-        vocab = vocab.union(set(doc))
+        for word in doc:
+            if word not in vocab: vocab.append(word)
     return vocab
 
-def compute_IDF(doc_list):
+def compute_IDF(doc_list, vocab):
     vocab = get_vocabulary(doc_list)
     # might need to initialize this
-    doc_counts = {}
+    doc_counts = []
     for word in vocab:
-        doc_counts[word] = _get_doc_count(word, doc_list)
-
+        doc_counts.append(_get_doc_count(word, doc_list))
+    
     # part of formula
-    idf_dict = {}
-    for word, count in doc_counts.items():
-        idf_dict[word] = _calc_idf(count, doc_list)
-    return idf_dict
+    idf_table = []
+    for count in doc_counts:
+        idf_table.append(_calc_idf(count, doc_list))
+    return np.array(idf_table)
 
 def _get_doc_count(word, doc_list):
     c = 0
