6c60ce8f39f4e43494fb8af29bfad7e1b440092f
ssulkar <ssulkar@calpoly.edu>
2018-06-08 04:41:17 -0700

    added three methods

---
+++ lab6/README.md
@@ -0,0 +1,12 @@
+#Lab 6
+
+Authors: Devon Martin, Shiv Sulkar
+Emails:
+- dmart112@calpoly.edu
+- ssulkar@calpoly.edu
+
+#Usage
+
+##Page Rank
+
+python3 pageRank.py <input_file>
--- lab6/lab6.py
+++ lab6/lab6.py
@@ -4,7 +4,6 @@ import numpy
 import math
 import random
 
-
 #IMPORTANT: user_ids and item_ids both start at id 0
 
 def main():
@@ -46,8 +45,12 @@ def main():
     #print(users)
     test_list = create_test(users, 1)
     print(test_list) 
-    weighted_sum(users, items,1,1)
+    user_id = test_list[0][0]
+    item_id = test_list[0][1]
+    actual = get_item_rating(users, user_id, item_id)
     
+    print(actual)
+    cosine_similarity_adjusted(users, items, user_id, item_id)
     
     
 def create_test(users, test_size):
@@ -76,21 +79,109 @@ def get_item_rating(users, user_id, item_id):
     return rating
 
 def weighted_sum(users, items, user_id, item_id):
-    sim = 0 
-    absim = 0
+    numer = 0 
+    denom = 0
     user = users[user_id]
     avg_user_rating = user["avg_user_rating"]
     
     for other_user in users:
         rating = other_user["user_ratings_list"][item_id]
+        #make sure the user isn't the same and also that their rating isn't null (99)
         if other_user["user_id"] != user_id and rating != 99.0:
+            
             sim = cosine(user["user_ratings_list"], other_user["user_ratings_list"])
-            print(sim)
+            #sim = pearson(user["user_ratings_list"], other_user["user_ratings_list"])
+            
+            denom += abs(sim)
+            numer += sim * (rating - avg_user_rating)
+    print(numer/denom)
+    return numer/denom
     
-def cosine(vec1, vec2):
-    numerator = numpy.dot(vec1, vec2) 
-    denominator = (math.sqrt(numpy.dot(vec1, vec1)) * math.sqrt(numpy.dot(vec2, vec2)))
-    return numerator/denominator    
+def default_voting(users, items, user_id, item_id):
+    numer = 0 
+    denom = 0
+    user = users[user_id]
+    avg_user_rating = user["avg_user_rating"]
     
+    for other_user in users:
+        rating = other_user["user_ratings_list"][item_id]
+        #make sure the user isn't the same and also that their rating isn't null (99)
+        if other_user["user_id"] != user_id and rating != 99.0:
+            sim = 1
+            n = 0
+            sum1 = 0
+            sum2 = 0
+            sq1 = 0
+            sq2 = 0
+            p = 0
+            rating_list1 = user["user_ratings_list"]
+            rating_list2 = other_user["user_ratings_list"]
+    
+            for i in range(len(rating_list1)):
+                rating1 = rating_list1[i]
+                rating2 = rating_list2[i]
+                if(rating1 != 99.0) and (rating2 != 99.0):
+                    n += 1
+                    sum1 += rating1
+                    sum2 += rating2
+                    sq1 += rating1 ** 2.0
+                    sq2 += rating2 ** 2.0
+                    p += rating1 * rating2
+            den = math.sqrt((sq1 - sum1**2 / n) * (sq2 - sum2**2 / n))
+
+            if den != 0:
+                #(1/k)
+                sim = p - (sum1*sum2 / n)/den
+    
+            denom += abs(sim)
+            numer += sim * (rating - avg_user_rating)
+    print(numer/denom)
+    return numer/denom    
+    
+def cosine_similarity_adjusted(users, items, user_id, item_id):
+    numer = 0 
+    denom = 0
+    user = users[user_id]
+    avg_user_rating = user["avg_user_rating"]
+    outsim = 0
+    
+    for other_user in users:
+        rating = other_user["user_ratings_list"][item_id]
+        #make sure the user isn't the same and also that their rating isn't null (99)
+        if other_user["user_id"] != user_id and rating != 99.0:
+            sim = 1
+            rating_list1 = user["user_ratings_list"]
+            rating_list2 = other_user["user_ratings_list"]
+            numerator = numpy.dot(rating_list1, rating_list2) 
+            denominator = (math.sqrt(numpy.dot(rating_list1, rating_list1)) * math.sqrt(numpy.dot(rating_list2, rating_list2)))
+            if(denominator != 0):
+                sim = numerator/denominator
+            denom += abs(sim)
+            numer += sim * (rating - avg_user_rating)
+    print(numer/denom)
+    return numer/denom
+    
+def cosine_similarity(users, items, user_id, item_id):
+    numer = 0 
+    denom = 0
+    user = users[user_id]
+    
+    for other_user in users:
+        rating = other_user["user_ratings_list"][item_id]
+        #make sure the user isn't the same and also that their rating isn't null (99)
+        if other_user["user_id"] != user_id and rating != 99.0:
+            sim = 1
+            rating_list1 = user["user_ratings_list"]
+            rating_list2 = other_user["user_ratings_list"]
+            numerator = numpy.dot(rating_list1, rating_list2) 
+            denominator = (math.sqrt(numpy.dot(rating_list1, rating_list1)) * math.sqrt(numpy.dot(rating_list2, rating_list2)))
+            if(denominator !=0):
+                sim = numerator/denominator
+            denom += abs(sim)
+            numer += sim * rating
+            
+    print(numer/denom)
+    return numer/denom
+  
 if __name__ == '__main__':
     main()
